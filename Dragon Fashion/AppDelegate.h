c8bba1fd905d8ae759e3fc56cfc2d5b3da4b46e2
mertakanay <mert.akanay@live.com>
2015-03-17 10:37:48 -0700

    Initial Commit

---
+++ Dragon Fashion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dragon Fashion
+//
+//  Created by Mert Akanay on 17.03.2015.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
